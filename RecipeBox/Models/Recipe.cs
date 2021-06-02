26f3f78ec04e5f7c59c557f947fb9a551f326ba8
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-06-02 12:41:23 -0800

    add Recipes Views
    co-authored-by: Min Chang <minchangmhc@gmail.com>

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -10,6 +10,7 @@ namespace RecipeBox.Models
       CategoryRecipes = new HashSet<CategoryRecipe>();
     }
     public string Name { get; set; }
+    public string Description { get; set; }
     public int RecipeId { get; set; }
     public int Rating { get; set; }
     public string Ingredient { get; set; }
