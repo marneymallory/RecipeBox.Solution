66ec0b08cf05eafb0da61bff0f49006aaeeae690
Min Chang <minchangmhc@gmail.com>
2021-06-03 10:59:50 -0700

    BUGFIX: users are now able to sign in
    Co-Authored-By: Marney Mallory <marney.mallory@gmail.com>

---
+++ RecipeBox/Migrations/20210603175218_Aspnet.cs
@@ -0,0 +1,430 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeBox.Migrations
+{
+    public partial class Aspnet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryRecipe_Categories_CategoryId",
+                table: "CategoryRecipe");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryRecipe_Recipes_RecipeId",
+                table: "CategoryRecipe");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_ApplicationUser_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryRecipe",
+                table: "CategoryRecipe");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CategoryRecipe_RecipeId_CategoryId",
+                table: "CategoryRecipe");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationUser",
+                table: "ApplicationUser");
+
+            migrationBuilder.RenameTable(
+                name: "CategoryRecipe",
+                newName: "CategoryRecipes");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicationUser",
+                newName: "AspNetUsers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryRecipe_CategoryId",
+                table: "CategoryRecipes",
+                newName: "IX_CategoryRecipes_CategoryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "varchar(256) CHARACTER SET utf8mb4",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "AspNetUsers",
+                type: "varchar(256) CHARACTER SET utf8mb4",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEmail",
+                table: "AspNetUsers",
+                type: "varchar(256) CHARACTER SET utf8mb4",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "varchar(256) CHARACTER SET utf8mb4",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryRecipes",
+                table: "CategoryRecipes",
+                column: "CategoryRecipeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    Name = table.Column<string>(type: "varchar(256) CHARACTER SET utf8mb4", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "varchar(256) CHARACTER SET utf8mb4", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    ClaimType = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    ClaimValue = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    ProviderKey = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    UserId = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    LoginProvider = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    Name = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    Value = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RoleId = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    ClaimType = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    ClaimValue = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    RoleId = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryRecipes_RecipeId",
+                table: "CategoryRecipes",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryRecipes_Categories_CategoryId",
+                table: "CategoryRecipes",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryRecipes_Recipes_RecipeId",
+                table: "CategoryRecipes",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_AspNetUsers_UserId",
+                table: "Recipes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryRecipes_Categories_CategoryId",
+                table: "CategoryRecipes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryRecipes_Recipes_RecipeId",
+                table: "CategoryRecipes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_AspNetUsers_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryRecipes",
+                table: "CategoryRecipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CategoryRecipes_RecipeId",
+                table: "CategoryRecipes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "CategoryRecipes",
+                newName: "CategoryRecipe");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                newName: "ApplicationUser");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryRecipes_CategoryId",
+                table: "CategoryRecipe",
+                newName: "IX_CategoryRecipe_CategoryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "ApplicationUser",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256) CHARACTER SET utf8mb4",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "ApplicationUser",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256) CHARACTER SET utf8mb4",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEmail",
+                table: "ApplicationUser",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256) CHARACTER SET utf8mb4",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ApplicationUser",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256) CHARACTER SET utf8mb4",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryRecipe",
+                table: "CategoryRecipe",
+                column: "CategoryRecipeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationUser",
+                table: "ApplicationUser",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryRecipe_RecipeId_CategoryId",
+                table: "CategoryRecipe",
+                columns: new[] { "RecipeId", "CategoryId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryRecipe_Categories_CategoryId",
+                table: "CategoryRecipe",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryRecipe_Recipes_RecipeId",
+                table: "CategoryRecipe",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_ApplicationUser_UserId",
+                table: "Recipes",
+                column: "UserId",
+                principalTable: "ApplicationUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- RecipeBox/Models/RecipeBoxContext.cs
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -1,22 +1,17 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace RecipeBox.Models
 {
-  public class RecipeBoxContext : DbContext
+  public class RecipeBoxContext : IdentityDbContext<ApplicationUser>
   {
-    public virtual DbSet<Category> Categories {get; set;}
-    public virtual DbSet<Recipe> Recipes {get; set;}
-    public virtual DbSet<CategoryRecipe> CategoryRecipes {get; set;}
-  public RecipeBoxContext(DbContextOptions options) : base(options){ }
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<Recipe> Recipes { get; set; }
+    public virtual DbSet<CategoryRecipe> CategoryRecipes { get; set; }
+    public RecipeBoxContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseLazyLoadingProxies();
-    }
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-      builder.Entity<CategoryRecipe>()
-          .HasIndex(cr => new { cr.RecipeId, cr.CategoryId })
-          .IsUnique();
     }
   }
 }
